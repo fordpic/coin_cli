9bfa59642a60753b53bae51c3a7e36d3a3303867
Ford Pickert <fordpickert@gmail.com>
2023-12-07 01:57:42 -0500

    add fetchers for trending and gainers

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const TRENDING_LATEST =
+	'pro-api.coinmarketcap.com/v1/cryptocurrency/trending/latest';
+export const GAINERS_N_LOSERS =
+	'pro-api.coinmarketcap.com/v1/cryptocurrency/trending/gainers-losers';
---
+++ src/fetcher/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { TRENDING_LATEST, GAINERS_N_LOSERS } from '../constants';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const CMC_KEY = process.env.CMC_API_KEY;
+
+export const fetchTrending = async () => {
+	try {
+		const response = await axios.get(TRENDING_LATEST, {
+			headers: {
+				'X-CMC_PRO_API_KEY': CMC_KEY,
+			},
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const fetchGainersNLosers = async () => {
+	try {
+		const response = await axios.get(TRENDING_LATEST, {
+			headers: {
+				'X-CMC_PRO_API_KEY': CMC_KEY,
+			},
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
